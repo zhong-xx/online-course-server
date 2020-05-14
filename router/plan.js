73a685e91ea9f80cc50fa4e91f1620a679d9e181
kerry <1457948957@qq.com>
2020-05-14 16:01:10 +0800

    制定了方案获取和新增功能

--- router/plan.js
+++ router/plan.js
@@ -1,28 +1,50 @@
 const express = require('express');
 const router = express.Router();
 const Plan = require('../model/Plan.js');
+const Course = require('../model/Course.js');
 
-router.post('/addPlanMessage', (req, res) => {
-    Plan.create({
-        major: req.body.major,
-        course_id: req.body.courseId
-    })
-    .then(()=> {
-        return res.json({
-            code: '0000',
-            msg: '添加方案成功'
-        })
-    })
-    .then(err => {
-        if(err.errors === null) 
-            return ;
-        for(let index in err.errors) {
-            return res.send({
-                code: '0001',
-                msg: err.errors[index].message
+router.get('/getCourseList', (req, res)=> {
+    Course.find()
+            .select('name')
+            .then(data=> {
+                data.forEach((item, index, arr)=> {
+                    item.type = '';
+                })
+                res.json({
+                    code: '0000',
+                    mag: '获取所有课程成功',
+                    data
+                })
             })
-        }
-    })
+})
+
+// 添加方案
+router.post('/addPlanMessage', (req, res) => {
+    Plan.find({major: req.body.major})
+        .then(data=> {
+            if(data.length > 0) {
+                return res.json({
+                    code: '0001',
+                    msg: '该专业已设定选课方案'
+                })
+            }
+            let array = [];
+            req.body.courseList.forEach(item=> {
+                let result = Plan.create({
+                    major: req.body.major,
+                    course_id: item._id
+                })
+                array.push(result);
+            })
+            Promise.all(array)
+                    .then(values=> {
+                        return res.json({
+                            code: '0000',
+                            msg: '添加方案成功'
+                        })
+                    })
+            
+        })
 })
 // [{major: '软件工程', courseList: ['dskf', '12354']}]
 router.get('/getPlanMessage', (req, res) => {
